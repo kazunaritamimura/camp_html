b2526b90fdec9b9f394ad0891021bee6550efdc1
kazunaritamimura <42229220+kazunaritamimura@users.noreply.github.com>
2021-03-06 14:32:03 +0900

    kadai1

---
+++ test.js
@@ -0,0 +1,12 @@
+$(".d1").on("click", function () {
+  $(".d1").html("吉岡道");
+});
+$(".f1").on("click", function () {
+  $(".f1").html("谷口ツキ子");
+});
+$(".h1").on("click", function () {
+  $(".h1").html("川村喜代江");
+});
+$(".e").on("click",function(){
+    $(".e").show(".i");
+}
